d592ebf22ff458570752c40fac960d9920fecd28
nikivivi9 <yingxi_kong@brown.edu>
2024-12-04 21:45:54 -0500

    script update

--- Project3/R/Data Simulation.R
+++ Project3/R/Data Simulation.R
@@ -3,41 +3,42 @@ library(tidyverse)
 #' Simulate data of normal distributed outcome
 #'
 #' @param G Number of clusters
-#' @param R Number of individuals in each cluster
 #' @param alpha Baseline mean value
 #' @param beta Treatment effect coefficient
 #' @param gamma_sq Variance between clusters
 #' @param sigma_sq Variance within clusters
 #' @param c1 Cost for the first sample in each cluster
-#' @param c2 Cost for any other samples in each cluster
+#' @param c1c2_ratio ratio of first sample cost and any other sample's cost (c1/c2)
 #' @param B Total budget
 #' @param niter Number of iterations
 #' @param folder Folder path to save the results data frame
-#' @param filename Filename format to save the results data frame
 #' @return Simulated dataframe
 
-simulate_data_normal <- function(G, R, alpha, beta, gamma_sq, sigma_sq, 
-                                 c1, c2, B, niter, folder, filename) {
+simulate_data_normal <- function(G, alpha, beta, gamma_sq, sigma_sq, 
+                                 c1, c1c2_ratio, B, niter, folder, filename) {
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
   
-  cost <- G * c1 + G * (R - 1) * c2
-  if (cost > B) {
+  total_cost <- G * c1 + G * (R - 1) * c2
+  if (total_cost > B) {
     return(NULL)
   }
   
   results_list <- vector("list", niter)
-  set.seed(2550)
   for (i in 1:niter) {
     repeat {
       X <- sample(c(0, 1), G, replace = TRUE)
       if (length(unique(X)) >= 2) break
     }
+    
     epsilon <- rnorm(G, 0, sd = sqrt(gamma_sq))
     df <- data.frame(cluster = rep(1:G, each = R),
                      treatment = rep(X, each = R),
                      Y = unlist(lapply(1:G, function(i) {
                        rnorm(R, mean = alpha + beta * X[i] + epsilon[i], 
-                             sd = sqrt(sigma_sq))
-                     })),
+                             sd = sqrt(sigma_sq))})),
                      G = G,
                      R = R,
                      alpha = alpha,
@@ -46,15 +47,16 @@ simulate_data_normal <- function(G, R, alpha, beta, gamma_sq, sigma_sq,
                      sigma_sq = sigma_sq,
                      c1 = c1,
                      c2 = c2,
+                     c1c2_ratio = c1c2_ratio,
                      B = B,
                      niter = i,
-                     total_cost = cost)
+                     total_cost = total_cost)
     
     results_list[[i]] <- df
   }
   
   long_df <- bind_rows(results_list)
-  write.csv(long_df, paste0(folder, filename, "_data.csv"), row.names = FALSE)
+  write.csv(long_df, paste0(folder, filename), row.names = FALSE)
   
   return(long_df)
 }
@@ -64,28 +66,31 @@ simulate_data_normal <- function(G, R, alpha, beta, gamma_sq, sigma_sq,
 #' Simulate data of poisson distributed outcome
 #'
 #' @param G Number of clusters
-#' @param R Number of individuals in each cluster
 #' @param alpha Baseline mean value
 #' @param beta Treatment effect coefficient
 #' @param gamma_sq Variance between clusters
 #' @param c1 Cost for the first sample in each cluster
-#' @param c2 Cost for any other samples in each cluster
+#' @param c1c2_ratio ratio of first sample cost and any other sample's cost (c1/c2)
 #' @param B Total budget
 #' @param niter Number of iterations
 #' @param folder Folder path to save the results data frame
-#' @param filename Filename format to save the results data frame
+#' @param format to store the results
 #' @return Simulated dataframe
 
-simulate_data_poisson <- function(G, R, alpha, beta, gamma_sq,
-                                  c1, c2, B, niter, folder, filename) {
+simulate_data_poisson <- function(G, alpha, beta, gamma_sq,
+                                  c1, c1c2_ratio, B, niter, folder, filename) {
   
-  cost <- G * c1 + G * (R - 1) * c2
-  if (cost > B) {
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
+  
+  total_cost <- G * c1 + G * (R - 1) * c2
+  if (total_cost > B) {
     return(NULL)
   }
   
   results_list <- vector("list", niter)
-  set.seed(2550)
   for (i in 1:niter) {
     repeat {
       X <- sample(c(0, 1), G, replace = TRUE)
@@ -102,50 +107,174 @@ simulate_data_poisson <- function(G, R, alpha, beta, gamma_sq,
                      alpha = alpha,
                      beta = beta,
                      gamma_sq = gamma_sq,
-                     sigma_sq = sigma_sq,
                      c1 = c1,
                      c2 = c2,
+                     c1c2_ratio = c1c2_ratio,
                      B = B,
                      niter = i,
-                     total_cost = cost)
+                     total_cost = total_cost)
     
     results_list[[i]] <- df
   }
   
   long_df <- bind_rows(results_list)
-  write.csv(long_df, paste0(folder, filename, "_data.csv"), row.names = FALSE)
+  write.csv(long_df, paste0(folder, filename), row.names = FALSE)
   
   return(long_df)
 }
 
+# Normal Distributed Y
 
-alpha = 0
-beta = 5
-gamma_sq = 1
-sigma_sq = 1
-B = 4650
-niter = 10
-c1 = 20
-c2 = 10
-G_list = c(5, 10, 15)
-R_list = c(10, 20, 30)
+## Vary G, R, c1/c2
+alpha <- 0
+beta <- 1.5
+gamma_sq <- 1
+sigma_sq <- 1
+B <- 10000
+niter <- 100
+c1 <- 20
+c1c2_ratio_list <- c(2, 5, 10, 20)
+G_list = c(5, 10, 15, 20, 25, 30)
+data_folder_normal <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Data_normal/"
+results_folder_normal <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Results_normal/"
+
+# Finding optimal design
+set.seed(2550)
 
 for (G in G_list) {
-  for (R in R_list) {
-    result_df <- simulate_data_normal(G, R, alpha, beta, gamma_sq, sigma_sq, c1, c2, B, niter = 10,
-                                       folder = "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Data_normal/",
-                                       filename = paste0("Normal", "_", G, "_", R))
-    
+  for (ratio in c1c2_ratio_list) {
+    result_df <- simulate_data_normal(G, alpha, beta, gamma_sq, sigma_sq, c1, ratio, B, niter,
+                                      folder = data_folder_normal,
+                                      filename = paste0("Normal", "_", G, "_", ratio, "_", alpha,
+                                                         "_", beta, "_", gamma_sq, "_", sigma_sq,
+                                                         "_data.csv"))
   }
 }
 
+
+## Vary gamma_sq
+
+alpha <- 0
+beta <- 1.5
+sigma_sq <- 1
+gamma_sq_list <- seq(0.5, 3, by = 0.5)
+B <- 10000
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+for (gamma_sq in gamma_sq_list) {
+  result_df <- simulate_data_normal(G, alpha, beta, gamma_sq, sigma_sq, c1, c1c2_ratio, B, niter,
+                                    folder = data_folder_normal, 
+                                    filename = paste0("Normal", "_", G, "_", c1c2_ratio, "_", alpha, 
+                                                      "_", beta, "_", gamma_sq, "_", sigma_sq,
+                                                      "_data.csv"))
+}
+
+
+## Vary sigma_sq
+
+alpha <- 0
+beta <- 1.5
+sigma_sq_list <- seq(0.5, 3, by = 0.5)
+gamma_sq <- 1
+B <- 10000
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+for (sigma_sq in sigma_sq_list) {
+  result_df <- simulate_data_normal(G, alpha, beta, gamma_sq, sigma_sq, c1, c1c2_ratio, B, niter,
+                                    folder = data_folder_normal, 
+                                    filename = paste0("Normal", "_", G, "_", c1c2_ratio, "_", alpha, 
+                                                      "_", beta, "_", gamma_sq, "_", sigma_sq,
+                                                      "_data.csv"))
+}
+
+
+# Poisson Distributed Y
+
+## vary G, R, c1/c2
+alpha <- 0
+beta <- 1.5
+gamma_sq <- 1
+B <- 10000
+niter <- 100
+c1 <- 20
+c1c2_ratio_list <- c(2, 5, 10, 20)
+G_list = c(5, 10, 15, 20, 25, 30)
+data_folder_poisson <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Data_poisson/"
+results_folder_poisson <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Results_poisson/"
+
 for (G in G_list) {
-  for (R in R_list) {
-    result_df <- simulate_data_poisson(G, R, alpha, beta, gamma_sq, c1, c2, B, niter = 10,
-                                      folder = "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Data_poisson/",
-                                      filename = paste0("Poisson", "_", G, "_", R))
-                                      
+  for (ratio in c1c2_ratio_list) {
+    result_df <- simulate_data_poisson(G, alpha, beta, gamma_sq, c1, ratio, B, niter,
+                                       folder = data_folder_poisson,
+                                       filename = paste0("Poisson", "_", G, "_", ratio, "_", alpha, 
+                                                         "_", beta, "_", gamma_sq, "_data.csv"))
   }
 }
+
+## Vary gamma_sq
+
+alpha <- 0
+beta <- 1.5
+B <- 10000
+gamma_sq_list <- seq(0.5, 3, by = 0.5)
+G <- 30
+R <- 32
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 2
+
+for (gamma_sq in gamma_sq_list) {
+  result_df <- simulate_data_poisson(G, alpha, beta, gamma_sq, c1, c1c2_ratio, B, niter,
+                                    folder = data_folder_poisson, 
+                                    filename = paste0("Poisson", "_", G, "_", c1c2_ratio, "_", alpha, 
+                                                      "_", beta, "_", gamma_sq, "_data.csv"))
+}
+
+## Vary alpha
+
+alpha_list <- seq(-3, 3, 1)
+beta <- 1.5
+B <- 10000
+gamma_sq <- 1
+G <- 25
+R <- 96
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+for (alpha in alpha_list) {
+  result_df <- simulate_data_poisson(G, alpha, beta, gamma_sq, c1, c1c2_ratio, B, niter,
+                                     folder = data_folder_poisson, 
+                                     filename = paste0("Poisson", "_", G, "_", c1c2_ratio, "_", alpha, 
+                                                       "_", beta, "_", gamma_sq, "_data.csv"))
+}
+
+## vary beta
+
+alpha <- 0
+beta_list <- c(-3, -2, -1, 1, 2, 3)
+B <- 10000
+gamma_sq <- 1
+G <- 25
+R <- 96
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+for (beta in beta_list) {
+  result_df <- simulate_data_poisson(G, alpha, beta, gamma_sq, c1, c1c2_ratio, B, niter,
+                                     folder = data_folder_poisson, 
+                                     filename = paste0("Poisson", "_", G, "_", c1c2_ratio, "_", alpha, 
+                                                       "_", beta, "_", gamma_sq, "_data.csv"))
+}
+
 
 
--- Project3/R/Experiment.R
+++ Project3/R/Experiment.R
@@ -1,4 +1,3 @@
-library(lmerTest)
 
 #' Modeling of normal distributed outcome
 #'
@@ -8,7 +7,7 @@ library(lmerTest)
 #' @param filename Filename format to save the results data frame
 #' @return Model results dataframe
 #' 
-normal_model <- function(data_path, niter, results_folder, filename) {
+normal_model <- function(data_path, beta, niter, results_folder, filename) {
   data <- read.csv(data_path)
   results_list <- vector("list", niter)
   
@@ -32,9 +31,23 @@ normal_model <- function(data_path, niter, results_folder, filename) {
   }
   
   results <- bind_rows(results_list)
-  write.csv(results, file = paste0(results_folder, filename, "_results.csv"), row.names = FALSE)
   
-  return(results)
+  coverage <- mean(results$beta_lower <= beta & results$beta_upper >= beta, na.rm = TRUE)
+  summary_metrics <- data.frame(G = mean(data$G), R = mean(data$R), c1 = mean(data$c1), 
+                                c2 = mean(data$c2), c1c2_ratio = mean(data$c1c2_ratio),
+                                alpha = mean(data$alpha), beta = beta, 
+                                gamma_sq = mean(data$gamma_sq), sigma_sq = mean(data$sigma_sq),
+                                total_cost = mean(data$total_cost),
+                                beta_estimate = mean(results$beta_estimate, na.rm = TRUE),
+                                beta_variance = var(results$beta_estimate, na.rm = TRUE),
+                                beta_bias = abs(mean(results$beta_estimate, na.rm = TRUE) - beta),
+                                mse = mean((results$beta_estimate - beta)^2, na.rm = TRUE),
+                                coverage = coverage * 100)
+  
+  write.csv(results, file = paste0(results_folder, filename), row.names = FALSE)
+  write.csv(summary_metrics, file = paste0(results_folder, "Summary_", filename), row.names = FALSE)
+  
+  return(summary_metrics)
 }
 
 #' Modeling of poisson distributed outcome
@@ -45,7 +58,7 @@ normal_model <- function(data_path, niter, results_folder, filename) {
 #' @param filename Filename format to save the results data frame
 #' @return Model results dataframe
 #' 
-poisson_model <- function(data_path, niter, results_folder, filename) {
+poisson_model <- function(data_path, beta, niter, results_folder, filename) {
   data <- read.csv(data_path)
   results_list <- vector("list", niter)
   
@@ -59,54 +72,253 @@ poisson_model <- function(data_path, niter, results_folder, filename) {
     beta_lower <- beta_estimate - 1.96 * beta_se
     beta_upper <- beta_estimate + 1.96 * beta_se
     random_effect_variance <- as.numeric(VarCorr(model)$cluster[1])
-    exp_beta_estimate <- exp(beta_estimate)
-    exp_beta_lower <- exp(beta_lower)
-    exp_beta_upper <- exp(beta_upper)
     
     results_list[[i]] <- data.frame(iter = i,
                                     beta_estimate = beta_estimate,
                                     beta_se = beta_se,
                                     beta_lower = beta_lower,
                                     beta_upper = beta_upper,
-                                    random_effect_variance = random_effect_variance,
-                                    exp_beta_estimate = exp_beta_estimate,
-                                    exp_beta_lower = exp_beta_lower,
-                                    exp_beta_upper = exp_beta_upper)
+                                    random_effect_variance = random_effect_variance)
   }
   
   results <- bind_rows(results_list)
-  write.csv(results, file = paste0(results_folder, filename, "_results.csv"), row.names = FALSE)
   
-  return(results)
+  coverage <- mean(results$beta_lower <= beta & results$beta_upper >= beta, na.rm = TRUE)
+  summary_metrics <- data.frame(G = mean(data$G), R = mean(data$R), c1 = mean(data$c1), 
+                                c2 = mean(data$c2), c1c2_ratio = mean(data$c1c2_ratio),
+                                alpha = mean(data$alpha), beta = beta, 
+                                gamma_sq = mean(data$gamma_sq), total_cost = mean(data$total_cost),
+                                beta_estimate = mean(results$beta_estimate, na.rm = TRUE),
+                                beta_variance = var(results$beta_estimate, na.rm = TRUE),
+                                beta_bias = abs(mean(results$beta_estimate, na.rm = TRUE) - beta),
+                                mse = mean((results$beta_estimate - beta)^2, na.rm = TRUE),
+                                coverage = coverage * 100)
+  
+  write.csv(results, file = paste0(results_folder, filename), row.names = FALSE)
+  write.csv(summary_metrics, file = paste0(results_folder, "Summary_", filename, ".csv"), row.names = FALSE)
+  
+  return(summary_metrics)
 }
 
-alpha = 0
-beta = 5
-gamma_sq = 1
-sigma_sq = 1
-B = 4650
-niter = 10
-c1 = 20
-c2 = 10
-G_list = c(5, 10, 15)
-R_list = c(10, 20, 30)
-
+# Vary G, R, c1/c2
+set.seed(2550)
+alpha <- 0
+beta <- 1.5
+gamma_sq <- 1
+sigma_sq <- 1
+B <- 10000
+niter <- 100
+c1 <- 20
+c1c2_ratio_list <- c(2, 5, 10, 20)
+G_list = c(5, 10, 15, 20, 25, 30)
 data_folder_normal <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Data_normal/"
 results_folder_normal <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Results_normal/"
+table_result_path <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Table Results/"
 
-for (G in G_list) {
-  for (R in R_list) {
-    data_path <- paste0(data_folder_normal, "Normal_", G, "_", R, "_data.csv")
-    normal_model(data_path, niter, results_folder = results_folder_normal, filename = paste0("Results_normal", "_", G, "_", R))
+ summary_metrics_df <- data.frame()
+ for (G in G_list) {
+   for (ratio in c1c2_ratio_list) {
+     c2 <- c1 / ratio
+     cost_per_cluster <- B/G
+     n_c2 <- floor((cost_per_cluster - c1)/c2)
+     R <- n_c2 + 1
+     data_path <- paste0(data_folder_normal, "Normal", "_", G, "_", ratio, "_",
+                         alpha, "_", beta, "_", gamma_sq, "_", sigma_sq, "_data.csv")
+     summary_metrics <- normal_model(data_path, beta, niter, results_folder = results_folder_normal,
+                                     filename = paste0("Results_normal", "_", G, "_", ratio, "_",
+                                                       alpha, "_", beta, "_", gamma_sq, "_", sigma_sq, ".csv"))
+     summary_metrics_df <- rbind(summary_metrics_df, summary_metrics)
   }
 }
+write.csv(summary_metrics_df, file = paste0(table_result_path, "summary_metrics_df", ".csv"), row.names = FALSE)
 
+## Vary gamma_sq
+alpha <- 0
+beta <- 1.5
+sigma_sq <- 1
+B <- 10000
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+gamma_sq_list <- seq(0.5, 3, by = 0.5)
+summary_metrics_df_gamma <- data.frame()
+for (gamma_sq in gamma_sq_list) {
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
+  data_path <- paste0(data_folder_normal, "Normal", "_", G, "_", c1c2_ratio, "_", 
+                      alpha, "_", beta, "_", gamma_sq, "_", sigma_sq, "_data.csv")
+  summary_metrics <- normal_model(data_path, beta, niter, results_folder = results_folder_normal,
+                                  filename = paste0("Results_normal", "_", G, "_", c1c2_ratio, "_",
+                                                    alpha, "_", beta, "_", gamma_sq, "_", sigma_sq, ".csv"))
+  summary_metrics_df_gamma <- rbind(summary_metrics_df_gamma, summary_metrics)
+}
+
+write.csv(summary_metrics_df_gamma, file = paste0(table_result_path, "summary_metrics_df_gamma", ".csv"), 
+          row.names = FALSE)
+
+## Vary sigma_sq
+
+alpha <- 0
+beta <- 1.5
+gamma_sq <- 1
+B <- 10000
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+
+sigma_sq_list <- seq(0.5, 3, by = 0.5)
+summary_metrics_df_sigma <- data.frame()
+for (sigma_sq in sigma_sq_list) {
+
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
+  data_path <- paste0(data_folder_normal, "Normal", "_", G, "_", c1c2_ratio, "_", 
+                      alpha, "_", beta, "_", gamma_sq, "_", sigma_sq, "_data.csv")
+  summary_metrics <- normal_model(data_path, beta, niter, results_folder = results_folder_normal,
+                                  filename = paste0("Results_normal", "_", G, "_", c1c2_ratio, "_",
+                                                    alpha, "_", beta, "_", gamma_sq, "_", sigma_sq, ".csv"))
+  summary_metrics_df_sigma <- rbind(summary_metrics_df_sigma, summary_metrics)
+}
+
+write.csv(summary_metrics_df_sigma, file = paste0(table_result_path, "summary_metrics_df_sigma", ".csv"), 
+          row.names = FALSE)
+
+# Poisson Distributed Y
+
+## Vary G, R, and c1/c2
+
+alpha <- 0
+beta <- 1.5
+gamma_sq <- 1
+sigma_sq <- 1
+B <- 10000
+niter <- 100
+c1 <- 20
+c1c2_ratio_list <- c(2, 5, 10, 20)
+G_list = c(5, 10, 15, 20, 25, 30)
 data_folder_poisson <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Data_poisson/"
 results_folder_poisson <- "C:/Users/yingx/OneDrive/Desktop/Fall 2024/PHP 2550/Practical-Data-Analysis-Portfolio/Project3/Results_poisson/"
 
+summary_metrics_df_poisson <- data.frame()
 for (G in G_list) {
-  for (R in R_list) {
-    data_path <- paste0(data_folder_poisson, "Poisson_", G, "_", R, "_data.csv")
-    poisson_model(data_path, niter, results_folder = results_folder_poisson, filename = paste0("Results_poisson", "_", G, "_", R))
+  for (ratio in c1c2_ratio_list) {
+    
+    c2 <- c1 / ratio
+    cost_per_cluster <- B/G
+    n_c2 <- floor((cost_per_cluster - c1)/c2)
+    R <- n_c2 + 1
+    data_path <- paste0(data_folder_poisson, "Poisson", "_", G, "_", ratio, "_", 
+                        alpha, "_", beta, "_", gamma_sq, "_data.csv")
+    summary_metrics <- poisson_model(data_path, beta, niter, results_folder = results_folder_poisson,
+                                     filename = paste0("Results_poisson", "_", G, "_", ratio, "_",
+                                                       alpha, "_", beta, "_", gamma_sq, ".csv"))
+    summary_metrics_df_poisson <- rbind(summary_metrics_df_poisson, summary_metrics)
   }
 }
+
+write.csv(summary_metrics_df_poisson, file = paste0(table_result_path, "summary_metrics_df_poisson", ".csv"), 
+          row.names = FALSE)
+
+## Vary gamma_sq
+
+alpha <- 0
+beta <- 1.5
+B <- 10000
+gamma_sq_list <- seq(0.5, 3, by = 0.5)
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+summary_metrics_df_poisson_gamma <- data.frame()
+for (gamma_sq in gamma_sq_list) {
+  
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
+  data_path <- paste0(data_folder_poisson, "Poisson", "_", G, "_", c1c2_ratio, "_", 
+                      alpha, "_", beta, "_", gamma_sq, "_data.csv")
+  summary_metrics <- poisson_model(data_path, beta, niter, results_folder = results_folder_poisson,
+                                   filename = paste0("Results_poisson", "_", G, "_", c1c2_ratio, "_",
+                                                     alpha, "_", beta, "_", gamma_sq, ".csv"))
+  summary_metrics_df_poisson_gamma <- rbind(summary_metrics_df_poisson_gamma, summary_metrics)
+}
+
+write.csv(summary_metrics_df_poisson_gamma, 
+          file = paste0(table_result_path, "summary_metrics_df_poisson_gamma", ".csv"), 
+          row.names = FALSE)
+
+## Vary alpha
+
+alpha_list <- seq(-3, 3, 1)
+beta <- 1.5
+B <- 10000
+gamma_sq <- 1
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+summary_metrics_df_poisson_alpha <- data.frame()
+for (alpha in alpha_list) {
+  
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
+  data_path <- paste0(data_folder_poisson, "Poisson", "_", G, "_", c1c2_ratio, "_", 
+                      alpha, "_", beta, "_", gamma_sq, "_data.csv")
+  summary_metrics <- poisson_model(data_path, beta, niter, results_folder = results_folder_poisson,
+                                   filename = paste0("Results_poisson", "_", G, "_", c1c2_ratio, "_",
+                                                     alpha, "_", beta, "_", gamma_sq, ".csv"))
+  summary_metrics_df_poisson_alpha <- rbind(summary_metrics_df_poisson_alpha, summary_metrics)
+}
+
+write.csv(summary_metrics_df_poisson_alpha, 
+          file = paste0(table_result_path, "summary_metrics_df_poisson_alpha", ".csv"), 
+          row.names = FALSE)
+
+## vary beta
+
+alpha <- 0
+beta_list <- c(-3, -2, -1, 1, 2, 3)
+B <- 10000
+gamma_sq <- 1
+G <- 30
+R <- 79
+niter <- 100
+c1 <- 20
+c1c2_ratio <- 5
+
+summary_metrics_df_poisson_beta <- data.frame()
+for (beta in beta_list) {
+  
+  c2 <- c1 / c1c2_ratio
+  cost_per_cluster <- B/G
+  n_c2 <- floor((cost_per_cluster - c1)/c2)
+  R <- n_c2 + 1
+  data_path <- paste0(data_folder_poisson, "Poisson", "_", G, "_", c1c2_ratio, "_", 
+                      alpha, "_", beta, "_", gamma_sq, "_data.csv")
+  summary_metrics <- poisson_model(data_path, beta, niter, results_folder = results_folder_poisson,
+                                   filename = paste0("Results_poisson", "_", G, "_", c1c2_ratio, "_",
+                                                     alpha, "_", beta, "_", gamma_sq, ".csv"))
+  summary_metrics_df_poisson_beta <- rbind(summary_metrics_df_poisson_beta, summary_metrics)
+}
+
+write.csv(summary_metrics_df_poisson_beta, 
+          file = paste0(table_result_path, "summary_metrics_df_poisson_beta", ".csv"), 
+          row.names = FALSE)
+
